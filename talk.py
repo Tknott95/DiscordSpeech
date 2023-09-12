20f2af52b327c69209c437cd884adc68962fe111
tknott95 <tknott95@hotmail.com>
2023-09-12 10:26:09 -0600

    DiscordSpeech

--- talk.py
+++ talk.py
@@ -1,45 +1,19 @@
-from diffusers import StableDiffusionXLPipeline
-# from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
-
-import torch
-
-from colors import clr
-
-from PIL import Image 
-import PIL 
-
-model_id = "stabilityai/stable-diffusion-2"
+from bark import SAMPLE_RATE, generate_audio, preload_models
+from scipy.io.wavfile import write as write_wav
+from IPython.display import Audio
+import os
 
 
-class Diffusion():
-    pipe = StableDiffusionXLPipeline.from_pretrained(
-      "stabilityai/stable-diffusion-xl-base-1.0",
-      torch_dtype=torch.float16, 
-      variant="fp16",
-      use_safetensors=True,
-      add_watermarker=False
-    )
-    # scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
-    # pipe = StableDiffusionPipeline.from_pretrained(
-    #   model_id, 
-    #   scheduler=scheduler, 
-    #   torch_dtype=torch.float16,
-    #   add_watermarker=False)
-    # When using torch >= 2.0, you can improve the inference speed by 20-30% with torch.compile. Simple wrap the unet with torch compile before running the pipeline:
+class Talk():
+  # download and load all models
+  # preload_models() (SEEING IF LOADING ONLY ON RUN HELPS WITH VRAM)
 
-    # thie breaks shit
-    # pipe.unet = torch.compile(pipe.unet, mode="reduce-overhead", fullgraph=True)
-    pipe.to("cuda:0")
-    pipe.enable_sequential_cpu_offload()
+  def run(self, text_prompt):
+    preload_models()
+    audio_array = generate_audio(text_prompt, history_prompt="v2/en_speaker_9")
 
-
-    # _input = input(clr.bar + "\n  Type what kind of image you would like\n:  " + clr.clear)
-
-    def run(self, file_name, _input):
-        # prompt = "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k"
-        image = self.pipe(prompt=_input).images[0]
-
-        image_to_send = image.save(f'{file_name}.jpg')
-        print(clr.cyan + f'\n  {file_name}.jpg MADE and SAVED \n' + clr.clear)
-
-        return image_to_send
+    # save audio to disk
+    write_wav("new.wav", SAMPLE_RATE, audio_array)
+      
+    # play text in notebook
+    Audio(audio_array, rate=SAMPLE_RATE)
